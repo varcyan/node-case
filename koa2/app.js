0a92747b18db5a265a1c525c5c1c7eacf67d9d4d
varcyan <varcyan@qq.com>
2018-09-21 15:41:40 +0800

    test use

--- koa2/app.js
+++ koa2/app.js
@@ -4,6 +4,18 @@ const Koa = require('koa');
 // 创建一个Koa对象(实例化)表示web app本身:
 const app = new Koa();
 
+app.use(async (ctx, next) => {
+    console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
+    await next(); // 调用下一个middleware
+});
+
+app.use(async (ctx, next) => {
+    const start = new Date().getTime(); // 当前时间
+    await next(); // 调用下一个middleware
+    const ms = new Date().getTime() - start; // 耗费时间
+    console.log(`Time: ${ms}ms`); // 打印耗费时间
+});
+
 // 对于任何请求，app将调用该异步函数处理请求：
 app.use(async (ctx, next) => {
     // ctx 是由koa传入的封装了request和response的变量
