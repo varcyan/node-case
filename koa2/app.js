94074f22032729c4f4aa398efbc905be95eced83
varcyan <varcyan@qq.com>
2018-09-21 10:16:56 +0800

    install Koa2

---
+++ koa2/app.js
@@ -0,0 +1,18 @@
+// 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
+const Koa = require('koa');
+
+// 创建一个Koa对象(实例化)表示web app本身:
+const app = new Koa();
+
+// 对于任何请求，app将调用该异步函数处理请求：
+app.use(async (ctx, next) => {
+    // ctx 是由koa传入的封装了request和response的变量
+    // next 是koa传入的将要处理的下一个异步函数
+    await next();
+    ctx.response.type = 'text/html';
+    ctx.response.body = '<h1>Hello, koa2!</h1>';
+});
+
+// 在端口3000监听:
+app.listen(3000);
+console.log('app started at port 3000...');
