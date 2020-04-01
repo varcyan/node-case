53799ee7b1c1d9aed40520c594ab8a46775a75ba
varcyan <varcyan@qq.com>
2020-04-01 18:17:11 +0800

    读取文件 同步readFileSync/异步readFile

--- 3.readFile.js
+++ 3.readFile.js
@@ -1,10 +1,16 @@
 //readfile.js
+// 异步
 var fs = require("fs");
 fs.readFile("assets/file.txt", "utf-8", function(err, data) {
     if (err) {
         console.error(err);
     } else {
-        console.log(data);
+        console.log(data + 'not sync');
     }
 });
 console.log("end.");
+
+// 同步
+var data1 = fs.readFileSync('assets/file.txt', 'utf-8');
+console.log(data1);
+console.log('end.'); 
