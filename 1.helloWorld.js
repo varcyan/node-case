a47c32f0af3c73dd661433ab23395388c01dca78
varcyan <varcyan@qq.com>
2020-04-01 17:12:29 +0800

    node服务器 读取文件

---
+++ 1.helloWorld.js
@@ -0,0 +1,3 @@
+// 打印 console 是 Node.js 提供的控制台对象
+// console.log('HelloWorld!')
+console.log('%s: %d', 'Hello', 25); 
---
+++ koa-router/app.js
@@ -0,0 +1,30 @@
+// 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
+const Koa = require('koa');
+
+// 注意require('koa-router')返回的是函数:
+const router = require('koa-router')();
+
+// 创建一个Koa对象(实例化)表示web app本身:
+const app = new Koa();
+
+// log request URL:
+app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    await next();
+});
+
+// add url-route:
+router.get('/hello/:name', async (ctx, next) => {
+    var name = ctx.params.name;
+    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+});
+
+router.get('/', async (ctx, next) => {
+    ctx.response.body = '<h1>Index</h1>';
+});
+
+// add router middleware:
+app.use(router.routes());
+
+app.listen(3000);
+console.log('app started at port 3000...');
