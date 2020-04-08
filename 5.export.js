91a4bc73087e72270489410824f39145d7c029d2
varcyan <varcyan@qq.com>
2020-04-08 10:31:31 +0800

    模块引入与导出 覆盖exports

--- 5.export.js
+++ 5.export.js
@@ -1,7 +1,21 @@
-var name
-exports.setName = function (val) {
-    name = val
+var name;
+exports.setName = function(val) {
+    name = val;
+};
+exports.sayHello = function() {
+    console.log("hello," + name);
+};
+
+function Hello() {
+    var name;
+    this.setName = function(thyName) {
+        name = thyName;
+    };
+    this.sayHello = function() {
+        console.log("Hello " + name);
+    };
 }
-exports.sayHello = function () {
-    console.log('hello,' + name);
-}
+// 需要通过require('moduleName').Hello读取
+// exports.Hello = Hello;
+// 引入后可以直接读取hello
+module.exports = Hello
--- 6.getModule.js
+++ 6.getModule.js
@@ -1,4 +1,14 @@
 // 与export.js在同一目录下
 var myModule = require('./5.export')
-myModule.setName('智')
-myModule.sayHello()
+// myModule.setName('智')
+// myModule.sayHello()
+
+// 单次加载
+var hello1 = require('./5.export');
+// hello1.setName('BYVoid');
+var hello2 = require('./5.export');
+// hello2.setName('BYVoid 2');
+// hello1.sayHello();  // BYVoid 2
+
+var he = require('./5.export')
+console.log(he)
